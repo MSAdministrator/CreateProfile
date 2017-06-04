1eb8fc88a2402642edded384b30183b8707e75eb
MSAdministrator <rickardja@live.com>
2017-06-03 21:04:24 -0400

    adding new functions to remote master;

---
+++ CreateProfile/CreateProfile.psm1
@@ -0,0 +1,30 @@
+﻿#requires -Version 2
+
+  <#
+  .SYNOPSIS
+    Loads all functions into memory
+  .DESCRIPTION
+    Loads all functions into memory and only exports the public functions for use
+  .NOTES
+    Author: Josh Rickard & Thom Schumacher
+    CreateDate: 04/01/2017 14:59:31
+  #>
+
+#Get public and private function definition files.
+$Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -Recurse -ErrorAction SilentlyContinue )
+$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -Recurse -ErrorAction SilentlyContinue )
+
+#Dot source the files
+Foreach($import in @($Public + $Private))
+{
+    Try
+    {
+        . $import.fullname
+    }
+    Catch
+    {
+        Write-Error -Message "Failed to import function $($import.fullname): $_"
+    }
+}
+
+Export-ModuleMember -Function $Public.Basename
---
+++ CreateProfile/Public/New-LocalUser.ps1
@@ -0,0 +1,121 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+.INPUTS
+   Inputs to this cmdlet (if any)
+.OUTPUTS
+   Output from this cmdlet (if any)
+.NOTES
+   General notes
+.COMPONENT
+   The component this cmdlet belongs to
+.ROLE
+   The role this cmdlet belongs to
+.FUNCTIONALITY
+   The functionality that best describes this cmdlet
+#>
+function New-LocalUser
+{
+    [CmdletBinding(DefaultParameterSetName='Parameter Set 1', 
+                  SupportsShouldProcess=$true, 
+                  PositionalBinding=$false,
+                  HelpUri = 'http://www.microsoft.com/',
+                  ConfirmImpact='Medium')]
+    [Alias()]
+    [OutputType([String])]
+    Param
+    (
+        # Username of the new local user account you want to create
+        [Parameter(Mandatory=$true, 
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=0)]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [Alias("name")] 
+        [string]$UserName,
+
+        # Param2 help description
+        [Parameter(Mandatory=$true, 
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=1)]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Password
+    )
+
+    Begin
+    {
+        # intentionally left blank
+    }
+    Process
+    {
+        if ($pscmdlet.ShouldProcess("$UserName on $env:COMPUTERNAME", "Creating Local User"))
+        {
+            try
+            {
+                $system = [ADSI]"WinNT://$env:COMPUTERNAME";
+                $user = $system.Create("user",$UserName);
+                $user.SetPassword($Password);
+                $user.SetInfo();
+ 
+                $flag=$user.UserFlags.value -bor 0x10000;
+                $user.put("userflags",$flag);
+                $user.SetInfo();
+ 
+                $group = [ADSI]("WinNT://$env:COMPUTERNAME/Users");
+                $group.PSBase.Invoke("Add", $user.PSBase.Path);
+            }
+            catch
+            {
+                Write-Warning
+            }
+        }
+    }
+    End
+    {
+        # intentionally left blank
+    }
+}
+
+
+#Function to create the new local user first
+function New-LocalUser
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $userName,
+        # Param2 help description
+        [string]
+        $password
+    )
+ 
+    $system = [ADSI]"WinNT://$env:COMPUTERNAME";
+    $user = $system.Create("user",$userName);
+    $user.SetPassword($password);
+    $user.SetInfo();
+ 
+    $flag=$user.UserFlags.value -bor 0x10000;
+    $user.put("userflags",$flag);
+    $user.SetInfo();
+ 
+    $group = [ADSI]("WinNT://$env:COMPUTERNAME/Users");
+    $group.PSBase.Invoke("Add", $user.PSBase.Path);
+}
